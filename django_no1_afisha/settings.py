a9eba0182d8544d03d20c0dfb9438885be7df380
SadRus <rystixx@yandex.ru>
2023-06-13 16:02:27 +0300

    Fix MEDIA_URL

--- django_no1_afisha/settings.py
+++ django_no1_afisha/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, os.environ['MEDIA_ROOT'])
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
