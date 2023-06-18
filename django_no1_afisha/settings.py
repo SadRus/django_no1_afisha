ac71929e2dbb18bb8fd3d44e1c8ba88d6dbccfbb
SadRus <rystixx@yandex.ru>
2023-06-18 22:30:12 +0300

    Add MEDIA-, STATIC_URL to enviroment variables

--- django_no1_afisha/settings.py
+++ django_no1_afisha/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, env('STATIC_ROOT'))
-STATIC_URL = '/static/'
+STATIC_URL = env('STATIC_URL')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, env('MEDIA_ROOT'))
-MEDIA_URL = 'media/'
+MEDIA_URL = env('MEDIA_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
